51b5a53a1a791ecd287e56d4bd29c87e2bc10404
Breedlove13 <brianbreedlove0013@gmail.com>
2021-07-02 00:52:00 -0700

    in the process of adding in categories

--- portfolio/components/nav/index.js
+++ portfolio/components/nav/index.js
@@ -1,9 +1,38 @@
 import React from "react"
 
+Const catagori
+
+
+
+
+function categorySelected() {
+  console.log("hello");
+}
+
+
 function Nav(){
-    return <header>
-        
-    </header>
+    return (
+      <header>
+        <h2>place to put a side logo or something</h2>
+        <nav>
+          <ul>
+            {" "}
+            className = "flex-flow">
+            <li className="m-2">
+              <a href="#about">About me</a>
+            </li>
+            <li>
+              <span>Contact</span>
+            </li>
+            {categories.map((category) => (
+              <li className="mx-1" key={category.name}>
+                <span onClick={categorySelected}>{category.name}</span>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </header>
+    );
 
 }
 
