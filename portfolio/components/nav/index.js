f4c37202a3b6df05feb06e4246c20a918bcf1af2
Breedlove13 <brianbreedlove0013@gmail.com>
2021-07-02 11:51:43 -0700

    basic nav bar

--- portfolio/components/nav/index.js
+++ portfolio/components/nav/index.js
@@ -1,1 +1,19 @@
+import React from "react";
 
+
+function Nav() {
+    <header>
+      <h1>
+        <div classname="logo"></div>
+      </h1>
+
+      <nav>
+        <li>Home</li>
+        <li>About Me</li>
+        <li> Projects</li>
+        <li>Contact</li>
+      </nav>
+    </header>;
+}
+
+export default Nav
