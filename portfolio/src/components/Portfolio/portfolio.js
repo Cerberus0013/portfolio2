4ee10155821c727704353b04f282f21df662fc98
Breedlove13 <brianbreedlove0013@gmail.com>
2021-07-04 10:48:33 -0700

    added better descriptions

--- portfolio/src/components/Portfolio/portfolio.js
+++ portfolio/src/components/Portfolio/portfolio.js
@@ -4,19 +4,19 @@ import React from "react";
 const portfolio = [
   {
     id: 1,
-    title: "testABC",
+    title: "Safer Sale",
     link: "deployed",
     github: "linktogithub",
-    image: "./assets/coverimg/download.jpg",
-    description: "jksdsjhsdhjsdjh"
+    image: "../../assets/safer/Screen Shot 2021-05-13 at 4.22.27 PM.png",
+    description: "This is an app designed to sell your unwanted goods and services in a safer manner, which provides suggestions on safe places to exchange your goods/services"
   },
   {
     id: 2,
-    title: "njbej",
-    link: "deployed",
-    github: "linktogithub",
+    title: "Daily Planner",
+    link: "website",
+    github: "repo",
     image: "./assets/planner/Screen Shot 2021-07-02 at 11.33.40 AM.png",
-    description: "vdvwwevewbwebwbwrb"
+    description: "This is a daily planner that helps plan your business day"
   },
 ];
 
