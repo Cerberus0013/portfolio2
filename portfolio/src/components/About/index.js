a50c1f7b2b5efcb0987ea3a55d1ff0e42f2b2369
Breedlove13 <brianbreedlove0013@gmail.com>
2021-07-04 13:42:51 -0700

    ff

--- portfolio/src/components/About/index.js
+++ portfolio/src/components/About/index.js
@@ -3,7 +3,7 @@ import React from "react";
 function About() {
   return (
     <section className="m-5">
-      <p id="about">My name is Brian Breedlove. I like to hike and and travel. I'm a new developer looking to expand my knowledge and skill level. </p>
+      <p id="about">My name is Brian Breedlove. I like to hike and and travel. I'm a new developer looking to expand my knowledge and skill level. I like to shoort Archery </p>
       
     </section>
   );
