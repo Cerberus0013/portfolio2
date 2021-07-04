d6fb9dca7051a3862ee487aa4aab3638e40c4e49
Breedlove13 <brianbreedlove0013@gmail.com>
2021-07-04 10:01:20 -0700

    getting ready to deploy because of time constraint

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,8 +3,9 @@ import './App.css';
 import React from "react";
 
 
-// import About from "./components/About/about";
+import About from "./components/About/index";
 import Header from "./components/Header/header";
+import Contact from "./components/Contact/index"
 import Footer from "./components/Footer/index";
 // import  from "./components/Projects/index.js"
 // import Resume from "./components/Resume/index.js"
@@ -16,7 +17,9 @@ function App() {
    <div>
      <Header/>
      <main>
+       <About/>
  <Portfolio/>
+  <Contact/>
     </main>
    <Footer/> 
    </div> 
--- portfolio/src/components/About/index.js
+++ portfolio/src/components/About/index.js
@@ -1,16 +1,10 @@
 import React from "react";
-import coverImage from "../../assets/cover/cover-image.jpg";
 
 function About() {
   return (
     <section className="m-5">
-      <h1 id="about">This is how we do it , its friday night</h1>
-      <img
-        src={coverImage}
-        classname="my-2"
-        style={{ width: "100%" }}
-        alt="cover"
-      />
+      <p id="about">My name is Brian Breedlove. I like to hike and and travel. I'm a new developer looking to expand my knowledge and skill level. </p>
+      
     </section>
   );
 }
